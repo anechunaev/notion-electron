f5956b471c108b4cd88e63deafa9e8b88979c458
Artem Nechunaev <artem@nechunaev.com>
2024-12-19 20:41:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Notion Electron
+
+Notion Electron is an unofficial desktop application for Notion, built using Electron. This project aims to provide a seamless and native-like experience for Notion users on desktop Linux (tested on Fedora 41).
+
+## Installation
+
+### Prerequisites
+
+- Node.js (v22 or higher)
+- npm (v10 or higher)
+
+### Steps
+
+1. Clone the repository:
+	```sh
+	git clone https://github.com/yourusername/notion-electron.git /usr/share/notion-electron
+	```
+2. Install dependencies:
+	```sh
+	npm install
+	```
+3. Build the application:
+	```sh
+	npm run make
+	```
+4. Install the application:
+	```sh
+	npm run install
+	```
+
+## Contributing
+
+Contributions are welcome! Please follow these steps to contribute:
+
+1. Fork the repository.
+2. Create a new branch (`git checkout -b feature-branch`).
+3. Make your changes.
+4. Commit your changes (`git commit -m 'Add some feature'`).
+5. Push to the branch (`git push origin feature-branch`).
+6. Open a pull request.
+
+## License
+
+This project is licensed under the MIT License.
+
+## Acknowledgements
+
+- [Electron](https://www.electronjs.org/)
+- [Notion](https://www.notion.so/)
---
+++ index.mjs
@@ -0,0 +1,125 @@
+import { app, BrowserWindow, shell, Tray, Menu, screen } from 'electron';
+import Store from 'electron-store';
+import { URL, fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const WIN_WIDTH = 1440 * 0.8;
+const WIN_HEIGHT = 900 * 0.8;
+
+let tray = null;
+let mainWindow = null;
+const store = new Store();
+
+function createWindow() {
+	const savedBounds = store.get('bounds');
+	const isMaximized = store.get('maximized', false);
+	const startLocation = store.get('location', '/');
+
+	const mainWindow = new BrowserWindow({
+		width: WIN_WIDTH,
+		height: WIN_HEIGHT,
+		icon: path.join(__dirname, '/assets/icons/desktop.png'),
+	});
+
+	if (isMaximized) {
+		mainWindow.maximize();
+	}
+
+	if (savedBounds !== undefined) {
+		const screenArea = screen.getDisplayMatching(savedBounds).workArea;
+		if (
+			(savedBounds.x > screenArea.x + screenArea.width || savedBounds.x < screenArea.x) ||
+			(savedBounds.y < screenArea.y || savedBounds.y > screenArea.y + screenArea.height)
+		) {
+			mainWindow.setBounds({
+				x: (screenArea.width - WIN_WIDTH) / 2,
+				y: (screenArea.height - WIN_HEIGHT) / 2,
+				width: WIN_WIDTH,
+				height: WIN_HEIGHT,
+			});
+		}
+		else {
+			mainWindow.setBounds(store.get('bounds'));
+		}
+	}
+
+	mainWindow.loadURL(`https://www.notion.com${startLocation}`);
+
+	mainWindow.webContents.setWindowOpenHandler(({ url }) => {
+		const u = new URL(url);
+
+		if (u.hostname === 'www.notion.com' || u.hostname === 'www.notion.so') {
+			return {
+				action: 'allow',
+				closeWithOpener: false,
+				overrideBrowserWindowOptions: undefined,
+			};
+		}
+
+		shell.openExternal(url);
+		return { action: 'deny' };
+	});
+
+	mainWindow.on('minimize', function (event) {
+		event.preventDefault();
+		mainWindow.hide();
+	});
+
+	mainWindow.on('close', function (event) {
+		const url = new URL(mainWindow.webContents.getURL());
+
+		store.set('location', url.pathname);
+		store.set('bounds', mainWindow.getBounds());
+		store.set('maximized', mainWindow.isMaximized());
+
+		if (!app.isQuiting) {
+			event.preventDefault();
+			mainWindow.hide();
+		}
+		return false;
+	});
+
+	return mainWindow;
+}
+
+function createTray() {
+	const tray = new Tray(path.join(__dirname, '/assets/icons/tray.png'));
+
+	const contextMenu = Menu.buildFromTemplate([
+		{
+			label: 'Show App',
+			click: function () {
+				mainWindow.show();
+			},
+		},
+		{
+			label: 'Quit',
+			click: function () {
+				app.isQuiting = true;
+				app.quit();
+			},
+		},
+	]);
+
+	tray.setToolTip('Notion');
+	tray.setContextMenu(contextMenu);
+
+	tray.on('click', () => {
+		mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();
+	});
+
+	return tray;
+}
+
+app.whenReady().then(() => {
+	mainWindow = createWindow();
+	tray = createTray();
+});
+
+app.on('window-all-closed', (event) => {
+	if (process.platform !== 'darwin') {
+		event.preventDefault();
+	}
+});
