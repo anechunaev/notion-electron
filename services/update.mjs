eeb0c379f4c386f5124ee676cd2d50e5d9e2627a
Artem Nechunaev <artem@nechunaev.com>
2025-03-30 18:21:48 +0200

    Fixed date formatting function

--- services/update.mjs
+++ services/update.mjs
@@ -240,11 +240,17 @@ class UpdateService extends EventEmitter {
 	}
 
 	#getSystemFormattedDate(dateString) {
-		const dateObject = dateString ? new Date(dateString) : new Date();
+		let dateObject = dateString ? new Date(dateString) : new Date();
+		if (isNaN(dateObject)) {
+			dateObject = new Date();
+		}
 		const isoString = dateObject.toISOString();
 		let dateStringFormatted;
 		try {
-			dateStringFormatted = execSync(`date -d ${isoString}`).toString().trim();
+			dateStringFormatted = execSync(
+				`date -d ${isoString}`,
+				{ stdio: ['pipe', 'pipe', 'ignore'], windowsHide: true }
+			).toString().trim();
 		} catch (error) {
 			dateStringFormatted = dateObject.toLocaleString();
 		}
