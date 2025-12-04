d0734fca2f969480e7e408523472a2a422213208
Artem Nechunaev <artem@nechunaev.com>
2025-12-04 22:21:32 +0100

    Added snap build to github workflow

--- dev/snap/build.sh
+++ dev/snap/build.sh
@@ -3,7 +3,7 @@
 jq '.build.executableName="notionelectron"' <<<$(<package.json) > package.json
 jq '.build.linux.target="snap"' <<<$(<package.json) > package.json
 
-npm run pack
+npm run pack -- --publish=always
 
 jq '.build.executableName="notion-electron"' <<<$(<package.json) > package.json
 jq '.build.linux.target=["rpm","deb","AppImage"]' <<<$(<package.json) > package.json
