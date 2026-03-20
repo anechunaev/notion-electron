7142fed9c6a94c59eeab1484c8f8d1c0641bdc23
Artem Nechunaev <artem@nechunaev.com>
2026-03-20 17:09:28 +0100

    Fixed issue with webview size on maximized window (issue #81)

--- services/windowPosition.mjs
+++ services/windowPosition.mjs
@@ -1,4 +1,4 @@
-import { screen } from 'electron';
+import { screen } from "electron";
 
 class WindowPositionService {
 	#window = null;
@@ -10,18 +10,20 @@ class WindowPositionService {
 	}
 
 	restorePosition() {
-		const savedBounds = this.#store.get('bounds');
-		const isMaximized = this.#store.get('maximized', false);
-	
+		const savedBounds = this.#store.get("bounds");
+		const isMaximized = this.#store.get("maximized", false);
+
 		if (isMaximized) {
 			this.#window.maximize();
 		}
-	
+
 		if (savedBounds !== undefined) {
 			const screenArea = screen.getDisplayMatching(savedBounds).workArea;
 			if (
-				(savedBounds.x > screenArea.x + screenArea.width || savedBounds.x < screenArea.x) ||
-				(savedBounds.y < screenArea.y || savedBounds.y > screenArea.y + screenArea.height)
+				savedBounds.x > screenArea.x + screenArea.width ||
+				savedBounds.x < screenArea.x ||
+				savedBounds.y < screenArea.y ||
+				savedBounds.y > screenArea.y + screenArea.height
 			) {
 				this.#window.setBounds({
 					x: (screenArea.width - screenArea.width * 0.8) / 2,
@@ -30,14 +32,14 @@ class WindowPositionService {
 					height: screenArea.height * 0.8,
 				});
 			} else {
-				this.#window.setBounds(this.#store.get('bounds'));
+				this.#window.setBounds(this.#store.get("bounds"));
 			}
 		}
 	}
 
 	savePosition() {
-		this.#store.set('bounds', this.#window.getBounds());
-		this.#store.set('maximized', this.#window.isMaximized());
+		this.#store.set("bounds", this.#window.getContentBounds());
+		this.#store.set("maximized", this.#window.isMaximized());
 	}
 }
 
